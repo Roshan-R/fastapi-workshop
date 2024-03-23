ae096e482c11fb8fa10c4ec76773d9e50a7f420b
Roshan-R <roshanr2001@gmail.com>
2024-03-24 01:15:35 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Request
 from pydantic import BaseModel
+from uuid import uuid4
+from contextlib import asynccontextmanager
+
 
 
 class Movie(BaseModel):
@@ -8,13 +11,28 @@ class Movie(BaseModel):
     rating: int
     director: str
 
+api_token = ""
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load the ML model
+    api_token = uuid4()
+    print("Api Token", api_token)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 movies = {
     "dune": Movie(name="Dune", id="dune", rating=9, director="Denis Villeneuve"),
     "avatar": Movie(name="Avatar", id="avatar", rating=8, director="James Cameron"),
 }
+
+
+async def authenticate_user(request: Request) -> bool:
+    print(request.headers, api_token)
+    return True
 
 
 @app.get("/")
@@ -25,7 +43,13 @@ async def root():
 async def get_all_movies() -> list[Movie]:
     return list(movies.values())
 
+@app.post("/movies/addnew")
+async def add_new_movie(movie: Movie) -> dict:
+    movies.update({movie.id: movie})
+    return {"success": True}
+
 @app.get("/movies/{id}")
-async def get_dune(id) -> Movie | dict:
+async def get_dune(id, is_authenticated: bool = Depends(authenticate_user)) -> Movie | dict:
     return movies.get(id, {"error": "Movie not found"})
+
 
